2cef81f11faf7b6cd44cc7079d6733e8ae1ccd7a
Unknown <guyuanhao@foxmail.com>
2018-05-31 15:39:57 +0800

    add routine maintenance module

---
+++ src/app/pages/routineMaintenance/routineMaintenance.module.js
@@ -0,0 +1,27 @@
+/**
+ * @author Leo GU
+ * created on 31.05.2018
+ */
+(function () {
+    'use strict';
+  
+    angular.module('BlurAdmin.pages.routineMaintenance', [])
+        .config(routeConfig);
+  
+    /** @ngInject */
+    function routeConfig($stateProvider) {
+      $stateProvider
+          .state('routineMaintenance', {
+            url: '/routineMaintenance',
+            title: '日常维修',
+            templateUrl: 'app/pages/routineMaintenance/routineMaintenance.html',
+            controller: 'routineMaintenanceCtrl',
+            sidebarMeta: {
+              icon: 'ion-android-home',
+              order: 0,
+            },
+          });
+    }
+  
+  })();
+  
---
+++ src/app/pages/routineMaintenance/routineMaintenanceCtrl.js
@@ -0,0 +1,17 @@
+/**
+ * @author Leo GU
+ * created on 31.05.2018
+ */
+(function () {
+    'use strict';
+  
+    angular.module('BlurAdmin.pages.routineMaintenance')
+      .controller('routineMaintenanceCtrl', routineMaintenanceCtrl);
+  
+    /** @ngInject */
+    function routineMaintenanceCtrl($scope) {
+      $scope.test = "Leo"
+    }
+  
+  })();
+  
